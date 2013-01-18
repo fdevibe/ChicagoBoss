233d6416d39d3b5f61b553b115b7c3695b5719e5
Fredrik de Vibe <fdevibe@gmail.com>
2013-01-18 02:35:59 +0100

    Make it possible to define extra variables per controller for injection into templates.

--- src/boss/boss_web_controller.erl
+++ src/boss/boss_web_controller.erl
@@ -760,19 +760,31 @@ execute_action({Controller, Action, Tokens} = Location, AppInfo, Req, SessionID,
                 OtherInfo ->
                     OtherInfo
             end,
+            InjectVars = case proplists:lookup("inject_vars_", ExportStrings) of
+                none ->
+                    [];
+                _ ->
+                    ControllerInstance:inject_vars_(Action)
+            end,
             case AuthInfo of
                 {ok, Info} ->
                     EffectiveRequestMethod = case Req:request_method() of
                         'HEAD' -> 'GET';
                         Method -> Method
                     end,
-                   ActionResult = case proplists:get_value(Action, ExportStrings) of
+                    ActionResult = case proplists:get_value(Action, ExportStrings) of
                         3 ->
                             ActionAtom = list_to_atom(Action),
-                            ControllerInstance:ActionAtom(EffectiveRequestMethod, Tokens);
+                            inject_extra_vars(
+                              ControllerInstance:ActionAtom(
+                                EffectiveRequestMethod, Tokens),
+                              InjectVars);
                         4 ->
                             ActionAtom = list_to_atom(Action),
-                            ControllerInstance:ActionAtom(EffectiveRequestMethod, Tokens, Info);
+                            inject_extra_vars(
+                              ControllerInstance:ActionAtom(
+                                EffectiveRequestMethod, Tokens, Info),
+                              InjectVars);
                         _ ->
                             undefined
                     end,
@@ -807,6 +819,19 @@ execute_action({Controller, Action, Tokens} = Location, AppInfo, Req, SessionID,
                     {redirect, process_redirect(Controller, Where, AppInfo)}
             end
     end.
+
+inject_extra_vars({ok, Data}, InjectVars) ->
+    {ok, Data ++ InjectVars};
+inject_extra_vars({ok, Data, L}, InjectVars) ->
+    {ok, Data ++ InjectVars, L};
+inject_extra_vars({render_other, OtherLocation}, InjectVars) ->
+    {render_other, OtherLocation, InjectVars};
+inject_extra_vars({render_other, OtherLocation, Data}, InjectVars) ->
+    {render_other, OtherLocation, Data ++ InjectVars};
+inject_extra_vars({render_other, OtherLocation, Data, Headers}, InjectVars) ->
+    {render_other, OtherLocation, Data ++ InjectVars, Headers};
+inject_extra_vars(Result, _) ->
+    Result.
 
 process_location(Controller,  [{_, _}|_] = Where, AppInfo) ->
     {_, TheController, TheAction, CleanParams} = process_redirect(Controller, Where, AppInfo),
